00ba37878ca703ea0f8f63ffe93b398432f2d804
Clodoaldo <clodoaldo@clo.com>
2019-05-19 16:53:17 -0400

    2

---
+++ precache-manifest.13745f6186f382324a4e0bff302b142f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737ab4ba93b4f18f286b2f2b2cddd98a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce0f05bf81ac25fb25f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "79c35a102e67f279e149",
+    "url": "/static/js/2.0b21358a.chunk.js"
+  },
+  {
+    "revision": "8ce0f05bf81ac25fb25f",
+    "url": "/static/js/main.1d53eeee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
