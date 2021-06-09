305d1bc868da06fda2fa997f7ecf08b6fc3021d2
Clodoaldo <clodoaldo@clo.com>
2021-06-08 20:42:37 -0400

    updates website

---
+++ precache-manifest.61a476a092efd64a91d91c0b199186e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b3d5e113eeb90e76442639c2cacd6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed743e5569a39751ac1a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e49cf38d5273b4559c8a",
+    "url": "/static/js/2.ffadcbc2.chunk.js"
+  },
+  {
+    "revision": "ed743e5569a39751ac1a",
+    "url": "/static/js/main.3b94fdc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
